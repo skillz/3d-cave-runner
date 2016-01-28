66143c2aaf01d4b995d8cc421b885710fb66a1e4
Dennis Zografos <dzog@skillz.com>
2016-01-28 10:41:06 -0500

    adding the Skillz wrapper

---
+++ CaveRunner/Assets/CaveRun3D/Scripts/SkillzDelegate.js
@@ -0,0 +1,11 @@
+#pragma strict
+
+// This method is called when a user starts a game from Skillz
+function SkillzMatchStarted () {
+	// implement me
+}
+
+// This method is called when a user exits the Skillz experience (via Menu -> Exit)
+function SkillzExited () {
+	// implement me
+}
