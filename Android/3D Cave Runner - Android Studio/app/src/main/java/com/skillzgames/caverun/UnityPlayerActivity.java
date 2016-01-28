fad6d879ffaca8fb8020357988634664344954f5
Dennis Zografos <dzog@skillz.com>
2016-01-28 13:21:13 -0500

    import Google Android Project into Android Studio

---
+++ Android/3D Cave Runner - Android Studio/app/src/main/java/com/skillzgames/caverun/UnityPlayerActivity.java
@@ -0,0 +1,8 @@
+package com.skillzgames.caverun;
+
+import com.unity3d.player.*;
+
+/**
+ * @deprecated Use UnityPlayerNativeActivity instead.
+ */
+public class UnityPlayerActivity extends UnityPlayerNativeActivity { }
---
+++ Android/3D Cave Runner - Android Studio/app/src/main/java/com/skillzgames/caverun/UnityPlayerProxyActivity.java
@@ -0,0 +1,24 @@
+package com.skillzgames.caverun;
+
+import com.unity3d.player.*;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * @deprecated Use UnityPlayerNativeActivity instead.
+ */
+public class UnityPlayerProxyActivity extends Activity
+{
+	protected void onCreate (Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		Intent intent = new Intent(this, com.skillzgames.caverun.UnityPlayerNativeActivity.class);
+		intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+		Bundle extras = getIntent().getExtras();
+		if (extras != null)
+			intent.putExtras(extras);
+		startActivity(intent);
+	}
+}
