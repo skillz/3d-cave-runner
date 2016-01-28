e203243dfe8019c87b6ff02a9715d22ad5e3bb77
Dennis Zografos <dzog@skillz.com>
2016-01-28 13:21:34 -0500

    remove Google Android Project (now that we have it as an Android Studio project)

--- Android/3D Cave Runner/src/com/skillzgames/caverun/UnityPlayerActivity.java
+++
@@ -1,8 +0,0 @@
-package com.skillzgames.caverun;
-
-import com.unity3d.player.*;
-
-/**
- * @deprecated Use UnityPlayerNativeActivity instead.
- */
-public class UnityPlayerActivity extends UnityPlayerNativeActivity { }
