9a434743dda59c167d71b3db2cae1ef2d2bc1e8c
Tj <tfallon@mail.depaul.edu>
2017-12-11 12:02:06 -0800

    Disable cash version of cave runner to speed up build jobs.

--- bin/build-cave-runner.sh
+++ bin/build-cave-runner.sh
@@ -17,8 +17,8 @@ sudo ln -sf /Users/opsadmin/Library/Android/sdk /usr/local/android-sdk-linux
 mv "${WORKSPACE}/themes/custom_theme-cr-vc.json" "${WORKSPACE}/Android/3D Cave Runner - Android Studio/app/src/main/assets/custom_theme.json"
 ./gradlew clean :app:assembleVconlyRelease :app:crashlyticsUploadDistributionVconlyRelease
 # Move theme for Full
-mv "${WORKSPACE}/themes/custom_theme-cr-full.json" "${WORKSPACE}/Android/3D Cave Runner - Android Studio/app/src/main/assets/custom_theme.json"
-./gradlew :app:assembleMainRelease :app:crashlyticsUploadDistributionMainRelease
+#mv "${WORKSPACE}/themes/custom_theme-cr-full.json" "${WORKSPACE}/Android/3D Cave Runner - Android Studio/app/src/main/assets/custom_theme.json"
+#./gradlew :app:assembleMainRelease :app:crashlyticsUploadDistributionMainRelease
 
 #####
 # Build VC and Full for iOS Crashlytics, and .xcarchives
@@ -67,53 +67,53 @@ mv "${WORKSPACE}/iOS/3D Cave Runner Xcode/build/Release-iphoneos/Cave Runner.app
 set -o pipefail && xcodebuild -sdk iphoneos -scheme VC -configuration Release clean archive \
 -archivePath "./Cave Runner" ONLY_ACTIVE_ARCH=NO BUILD_DIR=./build CODE_SIGN_IDENTITY="iPhone Distribution: Skillz Inc." | xcpretty
 
-# Move Custom theme for Full into Skillz Framework
-mv "${WORKSPACE}/themes/theme.json" "${WORKSPACE}/iOS/3D Cave Runner Xcode/Skillz.framework/theme.json"
+## Move Custom theme for Full into Skillz Framework
+#mv "${WORKSPACE}/themes/theme.json" "${WORKSPACE}/iOS/3D Cave Runner Xcode/Skillz.framework/theme.json"
 
-# Build Full for Crashlytics
-set -o pipefail && xcodebuild -sdk iphoneos -scheme Full -configuration Release clean build \
-ONLY_ACTIVE_ARCH=NO BUILD_DIR=./build CODE_SIGN_IDENTITY="iPhone Distribution: Skillz Inc." | xcpretty
-
-# Package Full Release Build
-rm -rf "${WORKSPACE}/PayloadFull/"
-mkdir -p "${WORKSPACE}/PayloadFull/"
-mv "${WORKSPACE}/iOS/3D Cave Runner Xcode/build/Release-iphoneos/3dCaveRun.app" "${WORKSPACE}/PayloadFull/"
-
-# Build Full .xcarchive
-set -o pipefail && xcodebuild -sdk iphoneos -scheme Full -configuration Release clean archive \
--archivePath ./3DCaveRunner ONLY_ACTIVE_ARCH=NO BUILD_DIR=./build CODE_SIGN_IDENTITY="iPhone Distribution: Skillz Inc." | xcpretty
-
-# Zip Archive for storing on Jenkins artifacts
-zip -y -r "Cave Runner.xcarchive.zip" "Cave Runner.xcarchive"
-zip -y -r 3DCaveRunner.xcarchive.zip 3DCaveRunner.xcarchive
+## Build Full for Crashlytics
+#set -o pipefail && xcodebuild -sdk iphoneos -scheme Full -configuration Release clean build \
+#ONLY_ACTIVE_ARCH=NO BUILD_DIR=./build CODE_SIGN_IDENTITY="iPhone Distribution: Skillz Inc." | xcpretty
+#
+## Package Full Release Build
+#rm -rf "${WORKSPACE}/PayloadFull/"
+#mkdir -p "${WORKSPACE}/PayloadFull/"
+#mv "${WORKSPACE}/iOS/3D Cave Runner Xcode/build/Release-iphoneos/3dCaveRun.app" "${WORKSPACE}/PayloadFull/"
+#
+## Build Full .xcarchive
+#set -o pipefail && xcodebuild -sdk iphoneos -scheme Full -configuration Release clean archive \
+#-archivePath ./3DCaveRunner ONLY_ACTIVE_ARCH=NO BUILD_DIR=./build CODE_SIGN_IDENTITY="iPhone Distribution: Skillz Inc." | xcpretty
+#
+## Zip Archive for storing on Jenkins artifacts
+#zip -y -r "Cave Runner.xcarchive.zip" "Cave Runner.xcarchive"
+#zip -y -r 3DCaveRunner.xcarchive.zip 3DCaveRunner.xcarchive
 
 # Remove archives
 rm -rf "iOS/3D Cave Runner Xcode/Cave Runner.xcarchive"
-rm -rf "iOS/3D Cave Runner Xcode/3DCaveRunner.xcarchive"
+#rm -rf "iOS/3D Cave Runner Xcode/3DCaveRunner.xcarchive"
 
 cd "${WORKSPACE}"
 
 # Create IPAs
-rm -rf "3DCaveRunner.ipa"
+#rm -rf "3DCaveRunner.ipa"
 rm -rf "Cave Runner.ipa"
 
 rm -rf "Payload"
 mv "PayloadVC" "Payload"
 zip -r "Cave Runner.ipa" "Payload"
 
-rm -rf "Payload"
-mv "PayloadFull" "Payload"
-zip -r "3DCaveRunner.ipa" "Payload"
+#rm -rf "Payload"
+#mv "PayloadFull" "Payload"
+#zip -r "3DCaveRunner.ipa" "Payload"
 
 
 # Store Dsym files
 unzip -q Skillz.framework.dSYM.zip -d "iOS/3D Cave Runner Xcode/build/Release-iphoneos"
 unzip -q Skillz_DEBUG.framework.dSYM.zip -d "iOS/3D Cave Runner Xcode/build/Debug-iphoneos"
 
-"${WORKSPACE}/iOS/3D Cave Runner Xcode/Crashlytics.framework/submit" 267045208f4b1d9fdcbf019068b81096fe16475a \
-bc1e89c576f18f877c98d2ca8a922096ef5415a8b5023e922eb6b2c474a455e1 \
--ipaPath "${WORKSPACE}/3DCaveRunner.ipa" \
--groupAliases SDK,qa-2,tournament-server,product
+#"${WORKSPACE}/iOS/3D Cave Runner Xcode/Crashlytics.framework/submit" 267045208f4b1d9fdcbf019068b81096fe16475a \
+#bc1e89c576f18f877c98d2ca8a922096ef5415a8b5023e922eb6b2c474a455e1 \
+#-ipaPath "${WORKSPACE}/3DCaveRunner.ipa" \
+#-groupAliases SDK,qa-2,tournament-server,product
 
 "${WORKSPACE}/iOS/3D Cave Runner Xcode/Crashlytics.framework/submit" 267045208f4b1d9fdcbf019068b81096fe16475a \
 bc1e89c576f18f877c98d2ca8a922096ef5415a8b5023e922eb6b2c474a455e1 \
