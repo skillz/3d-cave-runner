b97265386556149b003bb3268e313a9af5bd4356
Elizabeth Renn <erenn@skillz.com>
2018-03-29 13:45:53 -0700

    Build comment updated

--- bin/build-cave-runner.sh
+++ bin/build-cave-runner.sh
@@ -11,10 +11,8 @@ printf "apiSecret=bc1e89c576f18f877c98d2ca8a922096ef5415a8b5023e922eb6b2c474a455
 # Link Sidious location for Android SDK
 sudo ln -sf /Users/opsadmin/Library/Android/sdk /usr/local/android-sdk-linux
 # Compile Apps
-# Clean dependencies so latest Skillz SDK is fetched
-#./gradlew --refresh-dependencies
 
-# Move theme for VC, and compile
+# Move theme for VC, clean dependencies so latest Skillz SDK is fetched, and compile
 mv "${WORKSPACE}/themes/custom_theme-cr-vc.json" "${WORKSPACE}/Android/3D Cave Runner - Android Studio/app/src/main/assets/custom_theme.json"
 ./gradlew --refresh-dependencies clean :app:assembleVconlyRelease :app:crashlyticsUploadDistributionVconlyRelease
 
