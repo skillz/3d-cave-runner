c8361541171d3f60faa942fa932c35cd67a6e619
Liang Cheung <lcheung@skillz.com>
2018-04-11 10:41:02 -0700

    point to QA

--- iOS/3D Cave Runner Xcode/Libraries/Standard Assets/Skillz/Build/IncludeInXcode/Skillz+Unity.mm
+++ iOS/3D Cave Runner Xcode/Libraries/Standard Assets/Skillz/Build/IncludeInXcode/Skillz+Unity.mm
@@ -359,7 +359,7 @@ extern "C" void _skillzInitForGameIdAndEnvironment(const char *gameId, const cha
         [badEnvironmentException raise];
     }
     
-//    [[Skillz skillzInstance] performSelector:@selector(setDevTS) withObject:nil];
+    [[Skillz skillzInstance] performSelector:@selector(setDevTS) withObject:nil];
 //    [[Skillz skillzInstance] performSelector:@selector(setStaging)];
     [[Skillz skillzInstance] initWithGameId:gameIdString
                                 forDelegate:[[UnitySkillzSDKDelegate alloc] init]
