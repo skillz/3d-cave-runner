b5a99ee22d98669bb7297b62a82d2ff7c2568d98
Gabriel Firmacion <gabrielfirmacion@gmail.com>
2018-11-06 16:44:03 -0800

    force update

--- Android/3D Cave Runner - Android Studio/app/src/main/java/com/skillzgames/caverun/SkillzUnityPlayerActivity.java
+++ Android/3D Cave Runner - Android Studio/app/src/main/java/com/skillzgames/caverun/SkillzUnityPlayerActivity.java
@@ -1,6 +1,7 @@
 package com.skillzgames.caverun;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.crashlytics.android.Crashlytics;
 import com.skillz.activity.UnityGameActivity;
@@ -10,11 +11,11 @@ import io.fabric.sdk.android.Fabric;
 public class SkillzUnityPlayerActivity extends UnityGameActivity
 {
 	protected Crashlytics mCrashlytics;
-
 	// Setup activity layout
 	@Override protected void onCreate (Bundle savedInstanceState)
 	{
 		System.out.println("updating fingerprint");
+		Log.i("Skillz", "force update of fingerprint");
 		mCrashlytics = new Crashlytics();
 		Fabric.with(this, mCrashlytics);
 		super.onCreate(savedInstanceState);
