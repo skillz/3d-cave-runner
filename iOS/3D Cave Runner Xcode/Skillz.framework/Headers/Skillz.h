f11f650adc39f72bb5a15607c85276b611396941
Leo Leblanc <lleblancleo@gmail.com>
2019-03-04 19:31:16 -0800

    [#164344603] adding more updates to build this on jenkins

--- iOS/3D Cave Runner Xcode/Skillz.framework/Headers/Skillz.h
+++ iOS/3D Cave Runner Xcode/Skillz.framework/Headers/Skillz.h
@@ -5,7 +5,7 @@
 //  Copyright (c) 2014 Skillz. All rights reserved.
 //  http://skillz.com/
 //
-//  Version: 22.0.2.581
+//  Version: 22.0.2.633
 
 #import <Skillz/SkillzInstance.h>
 #import <Skillz/Skillz+Analytics.h>
