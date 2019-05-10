f7b4079ca4aed64873abafd93f47132a7cb09d00
Elizabeth Renn <erenn@skillz.com>
2019-05-10 09:34:41 -0700

    Set back to prod, bump version

--- iOS/3D Cave Runner Xcode/Libraries/Standard Assets/Skillz/Build/IncludeInXcode/Skillz+Unity.mm
+++ iOS/3D Cave Runner Xcode/Libraries/Standard Assets/Skillz/Build/IncludeInXcode/Skillz+Unity.mm
@@ -497,7 +497,7 @@ extern "C" void _skillzInitForGameIdAndEnvironment(const char *gameId, const cha
     /*************
      * Temporarily point to Staging
      *************/
-    [[Skillz skillzInstance] performSelector:@selector(setStaging)];
+//    [[Skillz skillzInstance] performSelector:@selector(setStaging)];
     
     SkillzEnvironment skillzEnvironment;
     
